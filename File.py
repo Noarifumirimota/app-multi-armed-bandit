c1776ba1fe6869bc7417230f989590af53607e34
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-22 22:21:19 +0200

    feat: :boom: Add data set

---
+++ File.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Set conversion rates & number of samples.
+conversionRates = [0.05, 0.12, 0.09]
+n = 1000
+l = len(conversionRates)
+
+# Data set.
+x = np.zeros((n, l))
+for i in range(n):
+    for j in range(l):
+        if np.random.rand() < conversionRates[j]:
+            x[i][j] = 1
