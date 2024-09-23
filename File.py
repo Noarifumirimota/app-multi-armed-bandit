615ca13ebdb12d2eacc58c85069162b3c81ece03
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-23 18:59:31 +0200

    feat: Add thompson sampling

--- File.py
+++ File.py
@@ -15,3 +15,23 @@ for i in range(n):
 # Winners and losers tables.
 nPosReward = np.zeros(l)
 nNegReward = np.zeros(l)
+
+# Thompson.
+for i in range(n):
+    selected = 0
+    maxRandom = 0
+    for j in range(l):
+        randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
+        if randomBeta > maxRandom:
+            maxRandom = randomBeta
+            selected = j
+    if x[i][selected] == i:
+        nPosReward[selected] += 1
+    else:
+        nNegReward[selected] += 1
+
+# Print best slot machine.
+nSelected = nPosReward + nNegReward
+for i in range(l):
+    print('Machine: ' + str(i + 1) + ' was selected: ' + str(nSelected[i]) + ' times.')
+print('Best machine is machine number: ' + str(np.argmax(nSelected) + 1))
