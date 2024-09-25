194e15af41c657b0bdb8504c840708d3b3850baa
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-25 23:14:08 +0200

    feat: Add next Thompson file

--- File2.py
+++ File2.py
@@ -5,7 +5,7 @@ import numpy as np
 conversionRates = [
     0.08, 0.25, 0.25, 0.17, 0.11
 ]
-N = 10
+N = 100
 d = len(conversionRates)
 
 # Data set.
@@ -16,3 +16,29 @@ for i in range(N):
             x[i][j] = 1
 
 print(x)
+
+# Winners and losers tables(for beta). How many times a specific slot machine return positive or negative.
+nPosReward = np.zeros(d)
+nNegReward = np.zeros(d)
+
+# Thompson stuff.
+for i in range(N):
+    selected = 0
+    maxRandom = 0
+    for j in range(d):
+        # Beta.
+        randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
+        if randomBeta > maxRandom:
+            maxRandom = randomBeta
+            selected = j
+
+    if x[i][selected] == i:
+        nPosReward[selected] += 1
+    else:
+        nNegReward[selected] += 1
+
+# Best machine.
+nSelected = nPosReward + nNegReward
+for i in range(d):
+    print('Machine: ' + str(i + 1) + ' was selected: ' + str(nSelected[i]) + ' times.')
+print('Best machine is machine number: ' + str(np.argmax(nSelected) + 1))
