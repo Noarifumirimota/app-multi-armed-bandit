3d99fe212d51e1dcfdf8a6729667c70aa75c3a42
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-27 11:40:30 +0200

    refactor: ToDo - pandas
    Conversion rates

--- ThompsonSampling.py
+++ ThompsonSampling.py
@@ -2,6 +2,17 @@
 import numpy as np
 import pandas as pd
 
-# Read CSV file.
-file = pd.read_csv('kn_data.csv')
-print(file.to_string())
+# Read CSV file and set conversion rates.
+conversionRates = pd.read_csv('kn_data.csv',skiprows=1001,nrows=1,usecols=range(2,71))
+
+# Set Number of samples.
+N = 1000
+d = len(conversionRates)
+
+# Data set.
+x = np.zeros((N, d))
+print('\n >> ' + conversionRates)
+# for i in range(N):
+#     for j in range(N):
+#         if np.random.rand() < conversionRates[j]:
+#             x[i][j] = 1
